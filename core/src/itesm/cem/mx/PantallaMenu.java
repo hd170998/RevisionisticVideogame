73b5bf1a3de7b8ed9288b5609e9138856e2471fe
Hector David Hernandez Rodriguez <hd170998@hotmail.com>
2018-10-10 19:19:31 -0500

    Forest map, prueba 1

--- core/src/itesm/cem/mx/PantallaMenu.java
+++ core/src/itesm/cem/mx/PantallaMenu.java
@@ -1,13 +1,11 @@
 package itesm.cem.mx;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
+
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
