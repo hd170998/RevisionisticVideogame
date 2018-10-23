8e51d9e463b8a12a80d58a084cfd483bb77e3b20
Hector David Hernandez Rodriguez <hd170998@hotmail.com>
2018-10-23 00:59:26 -0500

    pantalla creditos

--- core/src/itesm/cem/mx/PantallaCreditos.java
+++ core/src/itesm/cem/mx/PantallaCreditos.java
@@ -1,57 +1,41 @@
 package itesm.cem.mx;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Touchpad;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class PantallaCreditos extends Pantalla {
-    private Label label;
     private PantallaInicio pantallaInicio;
     private BitmapFont font;
     private Texture textFondo;
-    private Texture Title;
-    private Stage stage;
+    private float elapsedTime;
+    private Texture creditos;
     public PantallaCreditos(PantallaInicio pantallaInicio) { this.pantallaInicio = pantallaInicio;}
     @Override
     public void show() {
-
         textFondo = new Texture("Creditos/Credits_BG.png");
         //fondo
-        Title = new Texture("LogoSecondScreen.png");
-        stage = new Stage(vista);
-        font = new BitmapFont(Gdx.files.internal("Letra.fnt"));
-        //Credits
-        Label.LabelStyle labelStyle = new Label.LabelStyle();
-        labelStyle.font = font;
-        labelStyle.fontColor = Color.valueOf("#77ff72");
-        label = new Label("Credits", labelStyle);
-        stage.addActor(label);
-        //
-        Label.LabelStyle labelStyle1 = new Label.LabelStyle();
-        labelStyle1.font = font;
-        labelStyle1.fontColor = Color.valueOf("#72ffcb");
-        Label label1 = new Label("*The Team",labelStyle1);
-        label1 .setPosition(label.getWidth(), 6*ALTO/8);
-        label1.setAlignment(Align.center);
-        label.setPosition(ANCHO/2- label.getWidth()/2, ALTO-label.getPrefHeight());
-        label.setAlignment(Align.left);
-        stage.addActor(label1);
-        //Coders
-        Label.LabelStyle labelStyle2 = new Label.LabelStyle();
-        labelStyle2.font = font;
-        labelStyle2.fontColor = Color.valueOf("49fbff");
-        Label label2 = new Label("==> Coders:", labelStyle2);
-        label2.setPosition(label2.getWidth()/2, 5*ALTO/8);
-        label2.setAlignment(Align.left);
-        stage.addActor(label2);
-
-
+        creditos = new Texture("Creditos/ProvisionalCreditsImage.png");
+        elapsedTime = 0;
 
     }
 
@@ -60,11 +44,10 @@ public class PantallaCreditos extends Pantalla {
         borrarPantalla(0,0,1);
         batch.setProjectionMatrix(camara.combined);
         batch.begin();
+        elapsedTime += Gdx.graphics.getDeltaTime();
         batch.draw(textFondo,0,0);
-        batch.draw(Title,ANCHO-Title.getWidth(),ALTO-Title.getHeight());
-        stage.draw();
+        batch.draw(creditos, 0, -creditos.getHeight()/2+(60*elapsedTime));
         batch.end();
-
     }
 
     @Override
@@ -79,7 +62,6 @@ public class PantallaCreditos extends Pantalla {
 
     @Override
     public void dispose() {
-        stage.dispose();
 
     }
 }
