9f796d5163e610db8fae6b123cf5bb03fd2aaa83
AndreaIvonneVT <43389575+AndreaIvonneVT@users.noreply.github.com>
2018-11-18 12:19:27 -0600

    Add files via upload

---
+++ android/assets/MapLaboratory/FoundKeySprite2.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="FoundKeySprite2" tilewidth="64" tileheight="128" tilecount="6" columns="6">
+ <image source="FoundKeySprite2.png" width="384" height="128"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ android/assets/MapLaboratory/tilea5.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="tilea5" tilewidth="32" tileheight="32" tilecount="128" columns="8">
+ <image source="tilea5.png" width="256" height="512"/>
+ <tile id="0">
+  <properties>
+   <property name="Material" value="Solido"/>
+  </properties>
+ </tile>
+ <tile id="39">
+  <properties>
+   <property name="Material" value="Solido"/>
+  </properties>
+ </tile>
+</tileset>
